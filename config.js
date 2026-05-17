02071106fba30b35d1030f5bd3d67f3e4319e214
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-05-17 17:05:10 +0000

    Add configuration for PPG Solutions client environment

---
+++ config.js
@@ -0,0 +1,5 @@
+// PPG Solutions Global Client Environment Initialization Configuration
+window.PPG_CONFIG = {
+    SUPABASE_URL: "https://adkptfexitqutwjdexmf.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFka3B0ZmV4aXRxdXR3amRexmfIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMTczODYsImV4cCI6MjA5NDU5MzM4Nn0.8Zd4QLZb8aImw8ea9RzWkxMVCW5aydpeRaHd7mqJW44"
+};
