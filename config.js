99661f3a6caeedeedf4b4421653901cb46c80098
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-05-22 19:36:40 +0100

    Add environment configuration for PPG Trading Club

---
+++ config.js
@@ -0,0 +1,8 @@
+// PPG Trading Club - Environment Configuration
+
+window.PPG_CONFIG = {
+  SUPABASE_URL: "https://mxkdwjwqieidizhtnguk.supabase.co",
+
+  SUPABASE_ANON_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14a2R3andxaWVpZGl6aHRuZ3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NTczMjEsImV4cCI6MjA5NTAzMzMyMX0.mptb4EOKmxUw9NCk7XHNjwiSGWfwdq4PlUWBf6kYIYo"
+};
