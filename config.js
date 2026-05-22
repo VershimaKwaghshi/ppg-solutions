271a6be2aac7bd37577f616d39dc67959d9a8472
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-05-22 19:36:00 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// PPG Trading Club Configuration
-
-window.PPG_CONFIG = {
-  SUPABASE_URL: "https://mxkdwjwqieidizhtnguk.supabase.co",
-
-  SUPABASE_ANON_KEY:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14a2R3andxaWVpZGl6aHRuZ3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NTczMjEsImV4cCI6MjA5NTAzMzMyMX0.mptb4EOKmxUw9NCk7XHNjwiSGWfwdq4PlUWBf6kYIYo"
-};
